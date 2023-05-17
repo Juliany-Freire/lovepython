f8c45ae50ff42d48bca2996a4bddfe8bab5241b7
Juliany Freire <108228802+Juliany-Freire@users.noreply.github.com>
2023-05-16 22:45:58 -0300

    Update love.py

--- love.py
+++ love.py
@@ -19,11 +19,4 @@ goto(-120, -250)
 color("black")  
 write("FELIZ UM ANO DE NAMORO", align="center", font=("Arial", 20, "bold"))  
 
-penup()  
-goto(0, 100)  
-color("white")  
-write("I & J 17/05/2022", align="center", font=("Arial", 17, "bold"))  
-
-
-
 done() 
